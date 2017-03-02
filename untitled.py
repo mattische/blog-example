f2ab3770ad81154c66d98c0e0941e874fed82fc2
Mattias Schertell <schertell@gmail.com>
2017-03-02 15:41:00 +0100

    db con

--- untitled.py
+++ untitled.py
@@ -19,5 +19,22 @@ def index():
     return render_template("index.html", title="Index page", blog_posts=result)
 
 
+
+@app.route('/post/<postId>')
+def edit_post(postId):
+    con = mysql.connector.connect(user='root', password='cbRu8_=w', host='194.47.143.131',
+                                  database='blog')
+
+    cursor = con.cursor()
+
+    cursor.execute("SELECT id, heading, body, date FROM posts WHERE id="+postId)
+    res = cursor.fetchone()
+    cursor.close()
+    con.close()
+    return render_template("index.html")
+
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
