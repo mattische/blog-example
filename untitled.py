9e4c5a047c345cefd9ffaaedcc860dcb242b27af
Mattias Schertell <schertell@gmail.com>
2017-03-02 14:45:34 +0100

    sd<sdgfhc

--- untitled.py
+++ untitled.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import mysql.connector
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return 'It works!'
 
 
 if __name__ == '__main__':
