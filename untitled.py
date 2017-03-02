752d0e09c16253d50610d4db76b504aa30f59694
Mattias Schertell <schertell@gmail.com>
2017-03-02 15:59:09 +0100

    ok

--- untitled.py
+++ untitled.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    con = mysql.connector.connect(user='root', password='cbRu8_=w', host='194.47.143.131',
-                                  database='blog')
+    con = mysql.connector.connect(user='<user>', password='<password>', host='194.47.143.131',
+                                  database='<database name>')
 
     cursor = con.cursor()
     cursor.execute("SELECT id, heading, body, date FROM posts")
@@ -16,14 +16,14 @@ def index():
     cursor.close()
     con.close()
 
-    return render_template("index.html", title="Index page", blog_posts=result)
+    return render_template("index.html", title="Index page", <database name>_posts=result)
 
 
 
 @app.route('/post/<postId>')
 def edit_post(postId):
-    con = mysql.connector.connect(user='root', password='cbRu8_=w', host='194.47.143.131',
-                                  database='blog')
+    con = mysql.connector.connect(user='<user>', password='<password>', host='194.47.143.131',
+                                  database='<database name>')
 
     cursor = con.cursor()
 
@@ -31,10 +31,24 @@ def edit_post(postId):
     res = cursor.fetchone()
     cursor.close()
     con.close()
-    return render_template("index.html")
+    return render_template("edit_post.html", post=res)
 
 
+@app.route('/update_post', methods=['POST'])
+def update():
+    print("update post...")
+    con = mysql.connector.connect(user='<user>', password='<password>', host='194.47.143.131',
+                                  database='<database name>')
 
+    cursor = con.cursor()
+
+    cursor.execute("UPDATE posts SET heading='"+request.form['heading']+"', body='"+\
+                   request.form['body']+"', date=now() WHERE id=" + request.form['postId'])
+
+    cursor.close()
+    con.commit()
+    con.close()
+    return render_template("edited_post.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
